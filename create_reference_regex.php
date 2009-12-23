4f150b2a9d912d13f2a849e60429338fc90d3697
BibleForge <info@bibleforge.com>
2009-12-23 14:43:50 -0600

    Added the comment string and fixed a minor error with the first unused value in the array.

--- create_reference_regex.php
+++ create_reference_regex.php
@@ -1,17 +1,23 @@
 <?php
 
-require_once 'data/ref_array.php';
+$file = 'data/ref_array.php';
+
+require_once $file;
 require_once 'helpers/array2regex.php';
+
+$comment = "///NOTE: Created in the Forge via " . basename(__FILE__) . " on " . date('m-d-Y') . " from " . basename($file) . ".\n"; 
 
 $a2r = new array2regex();
 
 echo "<pre>";
+echo "$comment";
 echo 'var books_re = /^'.$a2r->convert($arr_all).'[\s0-9:.;,-]*$/i;'."\n";
 
-$str = 'var book_arr_re = ["",';
+$str = 'var book_arr_re = [0,';
 foreach ($arr as $value) {
 	$regex = $a2r->convert($value);
 	$str .= '/^' . $regex . '[\s0-9:.;,-]*$/i,';
 }
+
 echo substr($str, 0, -1);
 echo '];';
