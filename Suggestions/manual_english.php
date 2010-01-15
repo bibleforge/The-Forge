d3282fe5de051d3f9a16f7d673c7c0332bba831d
BibleForge <info@bibleforge.com>
2010-01-15 00:32:42 -0600

    Notes and more manual corrections.

--- Suggestions/manual_english.php
+++ Suggestions/manual_english.php
@@ -8,6 +8,7 @@ $proper_nouns = array();
 $proper_nouns['gods'] = 'God';
 $proper_nouns['lords'] = 'Lord';
 $proper_nouns['spirits'] = 'Spirit';
+$proper_nouns['the lords'] = 'The Lord'; /// Or should this be The LORD or the Lord?
 
 
 change_words($change_words);
