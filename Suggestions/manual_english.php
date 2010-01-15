c26f3e337926deebeb326f1b8185cd03c4fc3b95
BibleForge <info@bibleforge.com>
2010-01-15 05:35:05 -0600

    More manual additions.

--- Suggestions/manual_english.php
+++ Suggestions/manual_english.php
@@ -9,6 +9,16 @@ $proper_nouns['gods'] = 'God';
 $proper_nouns['lords'] = 'Lord';
 $proper_nouns['spirits'] = 'Spirit';
 $proper_nouns['the lords'] = 'The Lord'; /// Or should this be The LORD or the Lord?
+$proper_nouns['of the lords'] = 'of the Lord'; /// LORD?
+$proper_nouns['for the lords'] = 'for the Lord'; /// LORD?
+$proper_nouns['and the lords'] = 'and the Lord'; /// LORD?
+$proper_nouns['the lords and'] = 'the Lord and'; /// LORD?
+$proper_nouns['of gods'] = 'of God';
+$proper_nouns['for gods'] = 'For God'; /// for?
+$proper_nouns['gods of'] = 'God of';
+$proper_nouns['gods and'] = 'God and';
+$proper_nouns['gods which'] = 'God which';
+$proper_nouns['gods shall'] = 'God shall';
 
 
 change_words($change_words);
