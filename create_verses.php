a3f5a141ddd5bf2c9b1a7477a9c2ca26ea03d8e0
BibleForge <info@bibleforge.com>
2010-03-22 01:17:43 -0500

    create_verse.php now creates both bible_english_verses and bible_english_html tables (and quickly).

--- create_verses.php
+++ create_verses.php
@@ -12,12 +12,12 @@ $searchable = 'bible_english_verses';
 $simple = 'bible_english_html';
 
 $db = mysql_connect("localhost", "XXXXXXXXX", "XXXXXXXXXXXXXX");
-mysql_select_db("Bible", $db);
+mysql_select_db("bf", $db);
 
 mysql_query("SET NAMES utf8");
 
 
-$query = "SELECT id, verseID, book, chapter, verse, word, divine, quotation, added FROM $table WHERE word != '' ORDER BY id";
+$query = "SELECT id, verseID, book, chapter, verse, word, divine, red, implied FROM $table WHERE word != '' ORDER BY id";
 
 $res = mysql_query($query) or die(mysql_error() . "<br>". $query);
 $vID = 0;
@@ -25,35 +25,56 @@ $b = 0;
 $c = 0;
 $v = 0;
 $str = "";
+$str2 = "";
+$query = "";
+$query2 = "";
+
+
+$sql_intro1 = "INSERT INTO $simple (id, book, chapter, verse, words) VALUES ";
+$sql_intro2 = "INSERT INTO $searchable (id, book, chapter, verse, words) VALUES ";
+
 while ($row = mysql_fetch_assoc($res)) {
 	if ($vID != $row['verseID']) {
 		if ($vID > 0) {
-			$query = "INSERT INTO $simple (id, book, chapter, verse, words) VALUES
-			($vID, $b, $c, $v, '". addslashes(addslashes(substr($str, 0, -1))) ."')";
-			mysql_query($query) or die(mysql_error() . "<br>". $query);
+			$query .= "($vID, $b, $c, $v, '". addslashes(addslashes(substr($str, 0, -1))) ."'),";
+			$query2 .= "($vID, $b, $c, $v, '". addslashes(addslashes(substr($str2, 0, -1))) ."'),";
+			
+			if (strlen($query) > 700000) {
+				mysql_query($sql_intro1 . substr($query, 0, -1)) or die(__LINE__ . "<br>" . mysql_error() . "<br>". $sql_intro1 . substr($query, 0, -1));
+				mysql_query($sql_intro2 . substr($query2, 0, -1)) or die(__LINE__ . "<br>" . mysql_error() . "<br>". $sql_intro2 . substr($query2, 0, -1));
+				$query = "";
+				$query2 = "";
+			}
 		}
 		$vID = $row['verseID'];
 		$b = $row['book'];
 		$c = $row['chapter'];
 		$v = $row['verse'];
 		$str = "";
+		$str2 = "";
 	}
 	$str .= "<a";
-	if ($row['divine'] == 1 || $row['quotation'] == 1 || $row['added'] == 1) {
+	if ($row['divine'] == 1 || $row['red'] == 1 || $row['implied'] == 1) {
 		$class_str = '';
 		$str .= " class=";
 		if ($row['divine'] == 1) $class_str .= " d";
-		if ($row['quotation'] == 1) $class_str .= " q";
-		if ($row['added'] == 1) $class_str .= " a";
+		if ($row['red'] == 1) $class_str .= " q";
+		if ($row['implied'] == 1) $class_str .= " a";
 		$class_str = trim($class_str);
 		if (strpos($class_str, ' ') !== false) $class_str = "'$class_str'";
 		$str .= $class_str;
 	}
 	$str .= " id={$row["id"]}>{$row["word"]}</a> ";
+	$str2 .= "{$row["word"]} ";
 }
 
-$query = "INSERT INTO $simple (id, book, chapter, verse, words) VALUES
-		  ($vID, $b, $c, $v, '". addslashes(addslashes(substr($str, 0, -1))) ."')";
-mysql_query($query) or die(mysql_error() . "<br>". $query);
+$query .= "($vID, $b, $c, $v, '". addslashes(addslashes(substr($str, 0, -1))) ."')";
+$query2 .= "($vID, $b, $c, $v, '". addslashes(addslashes(substr($str2, 0, -1))) ."')";
+//mysql_query($query) or die(mysql_error() . "<br>". $query);
+mysql_query($sql_intro1 . $query) or die(__LINE__ . "<br>" . mysql_error() . "<br>". $sql_intro1 . $query);
+mysql_query($sql_intro2 . $query2) or die(__LINE__ . "<br>" . mysql_error() . "<br>". $sql_intro2 . $query2);
 
-echo 'done';
+
+?>
+done
+
