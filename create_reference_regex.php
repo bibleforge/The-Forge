c4d5137c5c1fd4d97aeea2aa1d81cfca56afe9b7
BibleForge <info@bibleforge.com>
2010-04-20 01:02:54 -0500

    Imporved create_reference_regex.php.
    Backslashed hyphens (-), added better whitespace, now writes one "var" statement.

--- create_reference_regex.php
+++ create_reference_regex.php
@@ -11,13 +11,13 @@ $a2r = new array2regex();
 
 echo "<pre>";
 echo "$comment";
-echo 'var books_re = /^'.$a2r->convert($arr_all).'[\s0-9:.;,-]*$/i;'."\n";
+echo 'var books_re = /^'.$a2r->convert($arr_all).'[\s0-9:.;,\-]*$/i,'."\n";
 
-$str = 'var book_arr_re = [0,';
+$str = '    book_arr_re = [0,';
 foreach ($arr as $value) {
 	$regex = $a2r->convert($value);
-	$str .= '/^' . $regex . '[\s0-9:.;,-]*$/i,';
+	$str .= ' /^' . $regex . '[\s0-9:.;,\-]*$/i,';
 }
 
 echo substr($str, 0, -1);
-echo '];';
+echo '],';
