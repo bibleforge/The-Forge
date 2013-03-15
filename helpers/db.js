1f8c9ec44babc4f756b37b4b2364fd29e543e376
BibleForge <info@bibleforge.com>
2013-03-16 02:22:46 +0800

    Added query_arry, fetch_rows, and made some improvements to db.js.

--- helpers/db.js
+++ helpers/db.js
@@ -14,6 +14,14 @@ this.db = (function ()
     db.connect({async: false});
     
     return {
+        fetch_rows: function (sql, callback)
+        {
+            var query = db.query();
+            
+            query.on("each", callback);
+            
+            query.execute(sql, []);
+        },
         query: function db_query(sql, callback)
         {
             db.query().execute(sql, [], function (err, data)
@@ -23,6 +31,39 @@ this.db = (function ()
                 }
             });
         },
+        query_arr: function db_query(sql_arr, callback)
+        {
+            var errs = [],
+                len  = sql_arr.length - 1,
+                res  = [];
+            
+            (function loop(i)
+            {
+                db.query().execute(sql_arr[i], [], function (err, data)
+                {
+                    if (err) {
+                        errs[i] = {
+                            err: err,
+                            sql: sql_arr[i]
+                        };
+                    }
+                    
+                    res[res.length] = data;
+                    
+                    if (i >= len) {
+                        if (typeof callback === "function") {
+                            callback(res, errs);
+                        }
+                    } else {
+                        /// Use a setTimeout to prevent too much recursion.
+                        setTimeout(function ()
+                        {
+                            loop(i + 1);
+                        }, 0);
+                    }
+                });
+            }(0));
+        },
         query_sync: function db_query(sql)
         {
             var res;
@@ -30,8 +71,8 @@ this.db = (function ()
             db.query().execute(sql, [], function (err, data)
             {
                 if (err) {
-                    console.log(err);
-                    console.log(sql);
+                    console.error(err);
+                    console.error(sql);
                 }
                 res = data;
             }, {async: false});
