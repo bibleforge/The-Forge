b9cb2d6c93c00f17141ee20ff628cf26f771de0d
BibleForge <info@bibleforge.com>
2013-03-16 02:22:18 +0800

    Added yes_no and color to ask.js.

--- helpers/ask.js
+++ helpers/ask.js
@@ -1,4 +1,8 @@
-this.ask = (function ()
+"use strict";
+
+var color = require("./color.js").color;
+
+var ask = (function ()
 {
     var callback;
     
@@ -9,12 +13,69 @@ this.ask = (function ()
         callback(chunk.substr(0, chunk.length - 1));
     });
     
-    return function (str, cb)
+    /**
+     * Ask the user for input.
+     *
+     * @param str   (string)                     The text to output to the user.
+     * @param def   (string) (optional)          The default input if nothing is entered.
+     * @param cb    (function)                   The function to call when data is returned.
+     * @param style (string || array) (optional) The color or style for the text (e.g., "red", "bold", ["blink", "blue", "bg_green"]).
+     */
+    return function (str, def, cb, style)
     {
-        callback = cb;
+        /// Is there no default answer?
+        if (typeof def === "function") {
+            callback = def;
+            style = cb;
+        /// There is a default answer variable, but it is undefined.
+        } else if (typeof def !== "undefined") {
+            /// Add the default text to the end of the string.
+            if (def) {
+                str += " (" + def + ") ";
+            }
+            /// Wrap the callback to add the default answer if necessary.
+            callback = function (res)
+            {
+                if (res.trim() === "") {
+                    res = def;
+                }
+                cb(res);
+            };
+        } else {
+            callback = cb;
+        }
+        
+        if (style) {
+            str = color(str, style);
+        }
+        
         process.stdout.write(str);
         
         process.stdin.setEncoding("utf8");
         process.stdin.resume();
     };
 }());
+
+this.yes_no = function (str, def, cb, style)
+{
+    var real_def;
+    
+    /// Is there no default answer?
+    if (typeof def === "function") {
+        style = cb;
+        cb = def;
+    } else {
+        real_def = def;
+    }
+    
+    ask(str, real_def, function (ans)
+    {
+        ans = String(ans).trim().toLowerCase();
+        
+        ans = (ans === "y" || ans === "ye" || ans === "yes" || ans === "yeah" || ans === "yep" || ans === "1" || ans === "true");
+        
+        cb(ans);
+    }, style);
+};
+
+this.ask = ask;
