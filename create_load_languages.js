d2b3d3fa9ec24619a94b4c15de81ea16f992215c
BibleForge <info@bibleforge.com>
2013-04-08 01:24:09 +0800

    Also set match_lang since it can be used before the languages are loaded.

--- create_load_languages.js
+++ create_load_languages.js
@@ -5,15 +5,18 @@ console.log("    /// Created in the Forge by create_load_languages.js.");
 fs.readdirSync(config.static_path + "js/lang/").forEach(function (filename)
 {
     var path = require("path"),
-        lang;
+        lang,
+        lang_obj;
     
     if (filename !== "en.js") {
         lang = path.basename(filename, path.extname(filename));
+        lang_obj = require(config.static_path + "js/lang/" + filename).BF.langs[lang];
         
         console.log("    if (!BF.lang." + lang + ") {");
         console.log("        BF.langs." + lang + " = {");
-        console.log("            full_name: \"" + require(config.static_path + "js/lang/" + filename).BF.langs[lang].full_name + "\",");
+        console.log("            full_name: \"" + lang_obj.full_name + "\",");
         console.log("            modified: 0,");
+        console.log("            match_lang: " + lang_obj.match_lang + ",");
         console.log("        };");
         console.log("    }");
     }
