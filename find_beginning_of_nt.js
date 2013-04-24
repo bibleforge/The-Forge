3386206c49d3e7d2fb40396ccb4b07eff2fd42a3
BibleForge <info@bibleforge.com>
2013-04-25 02:56:30 +0800

    Auto update the files.

--- find_beginning_of_nt.js
+++ find_beginning_of_nt.js
@@ -1,7 +1,9 @@
 var ask = require("./helpers/ask.js").ask,
-    db = require("./helpers/db.js").db;
+    config = require("./config.js").config,
+    db = require("./helpers/db.js").db
+    forge = require("./helpers/forge.js").forge;
 
-ask("Enter language: ", function(lang)
+function update(lang)
 {
     db.query("SELECT id FROM bible_" + lang + " WHERE verseID = 40001001 ORDER BY id LIMIT 1", function (data, err)
     {
@@ -10,6 +12,15 @@ ask("Enter language: ", function(lang)
             console.log("No results found.");
             return;
         }
+        forge(config.static_path + "/js/lang/" + lang + ".js", "New Testament Division", "nt: " + data[0].id + ",");
+        console.log("Updated " + lang + ".js");
         console.log(data[0].id);
+        process.exit();
     });
-});
+}
+
+if (process.argv.length === 3) {
+    update(process.argv[2]);
+} else {
+    ask("Enter language: ", update);
+}
