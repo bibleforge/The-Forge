1f4474595b1e6b27e9029469f9cdd2680d08ae7c
BibleForge <info@bibleforge.com>
2013-09-29 13:18:59 +0800

    Changed create_load_languages.js to handle the new hash format.

--- create_load_languages.js
+++ create_load_languages.js
@@ -16,7 +16,7 @@ require("fs").readdirSync(config.static_path + "js/lang/").forEach(function (fil
         lines[lines.length] = "if (!BF.lang." + lang + ") {";
         lines[lines.length] = "    BF.langs." + lang + " = {";
         lines[lines.length] = "        full_name: \"" + lang_obj.full_name + "\",";
-        lines[lines.length] = "        modified: 0,"; ///NOTE: We don't need to set "modified" here since it will be set by cache_buster.js.
+        lines[lines.length] = "        hash: \"0\","; ///NOTE: We don't need to set "modified" here since it will be set by cache_buster.js.
         lines[lines.length] = "        match_lang: " + lang_obj.match_lang + ",";
         lines[lines.length] = "    };";
         lines[lines.length] = "}";
