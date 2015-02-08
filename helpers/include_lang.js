51ddf0ef60282fcd08e3ad357ab5b97113e922b0
BibleForge <info@bibleforge.com>
2015-02-08 10:08:48 +0800

    Use path in include_lang.js.

--- helpers/include_lang.js
+++ helpers/include_lang.js
@@ -1,8 +1,9 @@
-var config = require("../config.js").config;
+var config = require("../config.js").config,
+    p = require("path");
 
 exports.include_lang = function include_lang(lang_id)
 {
-    var lang = require(process.cwd() + "/" + config.static_path + "/js/lang/" + lang_id + ".js").BF.langs,
+    var lang = require(p.join(process.cwd(), config.static_path, "js/lang", lang_id + ".js")).BF.langs,
         id;
     
     id = Object.keys(lang)[0];
